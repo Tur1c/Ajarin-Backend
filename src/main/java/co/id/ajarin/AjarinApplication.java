6c2292e202ba5b8f517e3ac0a3d36282a3d2142f
Ivander Gladwin <ivanderglad200@gmail.com>
2024-01-20 00:01:09 +0700

    first commit

---
+++ src/main/java/co/id/ajarin/AjarinApplication.java
@@ -0,0 +1,13 @@
+package co.id.ajarin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AjarinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AjarinApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/id/ajarin/AjarinApplicationTests.java
@@ -0,0 +1,13 @@
+package co.id.ajarin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AjarinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
